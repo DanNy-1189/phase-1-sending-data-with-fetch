3a75e6390b399d9d4aa70aaeac87cf865f312a7f
Nancy D. Arena <ndarena11@gmail.com>
2023-07-06 20:46:54 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData (name, email) {
+    const formData = {
+        name: "Steve",
+        email: "steve@steve.com",
+      };
+      
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+      
+      return fetch("http://localhost:3000/users", configurationObject)
+      .then(resp => resp.json())
+      .then(obj => document.body.innerHTML = obj.id)
+      .catch(error => document.body.innerHTML = error.message)
+}
+
